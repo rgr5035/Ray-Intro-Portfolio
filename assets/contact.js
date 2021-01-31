2de621d89185b2af713133140e00b38775c876c1
Robby Ray <rgr5035@gmail.com>
2021-01-31 16:04:00 -0700

    added functionality to contact page

--- assets/contact.js
+++ assets/contact.js
@@ -1,8 +1,46 @@
-const userEmail = document.getElementById("user-email");
-const userMessageText = document.getElementById("user-message");
-const submitBtn = document.getElementById("submit-btn");
+window.addEventListener("DOMContentLoaded", function() {
 
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("clicked");
-})
+    // get the form elements defined in your form HTML above
+    
+    const form = document.getElementById("contact-form");
+    // const button = document.getElementById("form-button");
+    const status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thank you for sharing!";
+    }
+
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
